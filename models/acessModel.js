2782850dd2e486cdad950165a9574ebf3e36d0f2
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-02-03 00:57:45 +0530

    some changes

---
+++ models/acessModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const RoleAccessSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  permissions: {
+    food_management: {
+      access: { type: Boolean, default: false },
+      add_menu: { type: Boolean, default: false },
+      add_addons: { type: Boolean, default: false },
+      add_recipe: { type: Boolean, default: false },
+      food_list: { type: Boolean, default: false },
+      addon_list: { type: Boolean, default: false },
+      recipe_list: { type: Boolean, default: false },
+    },
+    order_management: {
+      access: { type: Boolean, default: false },
+      order_list: { type: Boolean, default: false },
+      new_order: { type: Boolean, default: false },
+      bill_list: { type: Boolean, default: false },
+    },
+    inventory_management: {
+      access: { type: Boolean, default: false },
+      inventory_list: { type: Boolean, default: false },
+      add_inventory: { type: Boolean, default: false },
+    },
+  },
+});
+module.exports = mongoose.model("RoleAccess", RoleAccessSchema);
