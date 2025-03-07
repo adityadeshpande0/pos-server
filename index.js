9c207154942557522d45cc0a81ffe93f8dbbe419
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-03-08 00:49:24 +0530

    reverted

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 //CORS CONFIG
 const corsConfigurations = {
-  origin:"http://localhost:5000" || "*",
+  origin: process.env.CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
