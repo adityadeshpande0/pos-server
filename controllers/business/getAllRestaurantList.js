604495521fdb79bfb62b6e40d904d58ad14c89a9
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-03-12 11:43:45 +0530

    data response changes

--- controllers/business/getAllRestaurantList.js
+++ controllers/business/getAllRestaurantList.js
@@ -3,9 +3,31 @@ const BusinessAdmin = require("../../models/business/businessAdmin");
 
 const getAllRestaurantList = async (req, res) => {
   try {
-    const { businessAdminId } = req.params;
-    const restaurants = await RestaurantSchema.find({ businessAdmin: businessAdminId });
-    res.status(200).json(restaurants);
+    const businessAdminId = req.businessAdmin.id;
+    console.log(businessAdminId)
+    const restaurants = await RestaurantSchema.find({
+      businessAdminId,
+    });
+    res.status(200).json(
+      restaurants.map((restaurant) => {
+        return {
+          id: restaurant._id,
+          restaurantName: restaurant.restaurantName,
+          restaurantEmail: restaurant.restaurantEmail,
+          restaurantPhoneNumber: restaurant.phoneNumber,
+          gstNumber: restaurant.gstNumber,
+          restaurantProfilePicture: restaurant.profilePicture,
+          status: restaurant.status,
+          roleId: restaurant.roleId,
+          ownerName: restaurant.ownerName,
+          ownerEmail: restaurant.ownerEmail,
+          ownerPhoneNumber: restaurant.ownerPhoneNumber,
+          restaurantPermission:restaurant.permissions,
+          createdAt: restaurant.createdAt,
+          updatedAt: restaurant.updatedAt,
+        };
+      })
+    );
   } catch (error) {
     res
       .status(500)
